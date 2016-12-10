4bf69776e290e8acb947c43b4d67651f346fffdf
Lily Huo <yongjiahuo9210@gmail.com>
2016-12-10 15:50:17 -0500

    read and reformat the data, get the output profile

---
+++ personality_insights.py
@@ -0,0 +1,36 @@
+import json
+from watson_developer_cloud import PersonalityInsightsV3
+
+
+def json_export(data, name):
+    jsonFile = open(name + "_profile.json", "w")
+    jsonFile.write(json.dumps(data, indent=4, separators=(',', ': ')))
+    jsonFile.close()
+
+if __name__ == "__main__":
+    personality_insights = PersonalityInsightsV3(
+        version='2016-10-20',
+        username='f869d757-c85d-4372-8df2-503f5b41d365',
+        password='siUQ7mhItTJq')
+
+    min_words = 100
+    filename = 'EBay.json'
+    input_file = open(filename).read()
+    reviews = json.loads(input_file)
+    results = []
+    for review in reviews:
+        txt = ''
+        if review['advice'] != None:
+            txt += review['advice']
+        if review['cons'] != None:
+            txt += review['cons']
+        if review['pros'] != None:
+            txt += review['pros']
+        if review['summary'] != None:
+            txt += review['summary']
+        if len(txt.split()) > min_words:
+            profile = personality_insights.profile(
+                txt.encode('utf8'), content_type='text/plain',
+                raw_scores=True, consumption_preferences=True)
+            results.append(profile)
+    json_export(results, filename)
