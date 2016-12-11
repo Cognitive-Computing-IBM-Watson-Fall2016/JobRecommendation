5d467807a9c955301dfad70a31132080f3c9703c
Lily Huo <yongjiahuo9210@gmail.com>
2016-12-10 21:51:09 -0500

    double the text

--- personality_insights.py
+++ personality_insights.py
@@ -1,6 +1,6 @@
 import json
 from watson_developer_cloud import PersonalityInsightsV3
-
+from watson_developer_cloud import watson_developer_cloud_service
 
 def json_export(data, name):
     jsonFile = open(name + "_profile.json", "w")
@@ -10,11 +10,11 @@ def json_export(data, name):
 if __name__ == "__main__":
     personality_insights = PersonalityInsightsV3(
         version='2016-12-10',
-        username='e65927a0-94cc-4d27-b254-56e9d072e32d',
-        password='FnaMoGx8mbp5')
-
+        username='c53a4649-73dd-4960-af82-47681aa8637a',
+        password='OnSSzjEwGRsF')
+    max_input = 20
     min_words = 100
-    filename = 'EBay.json'
+    filename = 'microsoft.json'
     input_file = open(filename).read()
     reviews = json.loads(input_file)
     results = []
@@ -28,11 +28,17 @@ if __name__ == "__main__":
             txt += review['pros']
         if review['summary'] != None:
             txt += review['summary']
-
+        txt = txt + ' ' + txt
         if len(txt.split()) > min_words:
-            profile = personality_insights.profile(
-                txt.encode('utf8'), content_type='text/plain',
-                raw_scores=True, consumption_preferences=True)
-            profile['rating'] = review['rating']
-            results.append(profile)
+            try:
+                profile = personality_insights.profile(
+                    txt.encode('utf8'), content_type='text/plain',
+                    raw_scores=True, consumption_preferences=True)
+                profile['rating'] = review['rating']
+                results.append(profile)
+                if len(results) == max_input:
+                    break
+            except watson_developer_cloud_service.WatsonException:
+                continue
+    print filename, ' -- ', len(results)
     json_export(results, filename)
