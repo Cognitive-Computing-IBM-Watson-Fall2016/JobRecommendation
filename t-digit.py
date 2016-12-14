4e7b7e2101f531eb8525a5c9f01b1f5bb418d320
Lily Huo <yongjiahuo9210@gmail.com>
2016-12-14 14:53:31 -0500

    data

--- t-digit.py
+++ t-digit.py
@@ -1,20 +1,34 @@
 import json
+import numpy as np
+import pprint
 
-filename = 'Google.json'
-input_file = open(filename).read()
-reviews = json.loads(input_file)
-cnt = 0
-min_words = 50
-for review in reviews:
-    txt = ''
-    if review['advice'] != None:
-        txt += review['advice']
-    if review['cons'] != None:
-        txt += review['cons']
-    if review['pros'] != None:
-        txt += review['pros']
-    if review['summary'] != None:
-        txt += review['summary']
-    if len(txt.split()) > min_words:
-        cnt += 1
-print cnt
+if __name__ == "__main__":
+    min_words = 100
+    filename = 'test_data.json'
+    input_file = open(filename).read()
+    profile = json.loads(input_file)
+    X = []
+    y = []
+    need = []
+    value = []
+    person = []
+    for facet in profile['tree']['children']:
+        if facet['id'] == 'personality':
+            pprint.pprint()facet
+            for item in facet['children']:
+                person.append(item['percentage'])
+        if facet['id'] == 'needs':
+            for item in facet['children']:
+                need.append(item['percentage'])
+        if facet['id'] == 'values':
+            for item in facet['children']:
+                value.append(item['percentage'])
+    X.append(need)
+    X.append(person)
+    X.append(value)
+    print 'need', need
+    print 'person', person
+    print 'value', value
+    print 'X', X
+    x = np.array(X)
+    print x
