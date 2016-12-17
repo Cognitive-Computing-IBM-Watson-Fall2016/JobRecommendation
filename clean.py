2dd967605893d1f962efc1a86ac3fdc77e1cd363
Lily Huo <yongjiahuo9210@gmail.com>
2016-12-16 21:23:01 -0500

    connect the files

--- clean.py
+++ clean.py
@@ -4,25 +4,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 if __name__ == "__main__":
 
-    input_file = open('final_format.json').read()
+    input_file = open('vectors.json').read()
     companies = json.loads(input_file)  # dict
     company_scores = []                 # list of 2-tuples
 
-    user_vector = [3, 4, 5]
+    user_file = open('person_tweet.json').read()
+    user_vector = json.loads(user_file)['807677389878726657']
     user_vector = np.array(user_vector).reshape(1,-1)
 
     for company_name, company_vectors in companies.items():
-
         company_vectors = np.array(company_vectors)
-
         similarities = cosine_similarity(company_vectors, user_vector)
-
         company_score = 1 * similarities[0] + \
                         2 * similarities[1] + \
                         3 * similarities[2] + \
                         4 * similarities[3] + \
                         5 * similarities[4]
-
         company_scores.append((company_name, float(company_score)))
 
     company_scores.sort(key=lambda x: x[1])  # sort by score in descending order
@@ -30,7 +27,7 @@ if __name__ == "__main__":
 
     print company_scores[0][0]
     print company_scores[1][0]
-    #print company_scores[2][0]
-    #print company_scores[3][0]
+    print company_scores[2][0]
+    print company_scores[3][0]
     #print company_scores[4][0]
 
