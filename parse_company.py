e1191e0b48c01964f7b25a423a5f3d6a998bedae
Lily Huo <yongjiahuo9210@gmail.com>
2016-12-16 20:17:01 -0500

    analyze the input data and generate vectors

--- parse_company.py
+++ parse_company.py
@@ -1,12 +1,11 @@
 import json
 from watson_developer_cloud import PersonalityInsightsV3
-from watson_developer_cloud import watson_developer_cloud_service
-import pprint
+
 min_words = 100
 
 
 def json_export(data):
-    jsonFile = open("data_analyzed.json", "w")
+    jsonFile = open("data_analyzed_1.json", "w")
     jsonFile.write(json.dumps(data, indent=4, separators=(',', ': ')))
     jsonFile.close()
 
@@ -25,52 +24,43 @@ def to_vector(profile):
     return X
 
 
-def get_personality(summ):
+def get_personality(rev):
     personality_insights = PersonalityInsightsV3(
         version='2016-12-10',
         username='c53a4649-73dd-4960-af82-47681aa8637a',
         password='OnSSzjEwGRsF')
     result = []
-    for i in range(0, 5):
-        txt = summ[i]
-        if len(txt.split()) > min_words:
-            try:
-                profile = personality_insights.profile(
-                    txt.encode('utf8'), content_type='text/plain',
-                    raw_scores=True, consumption_preferences=True)
-                result.append(to_vector(profile))
-            except watson_developer_cloud_service.WatsonException:
-                continue
-        else:
-            result.append([])
+    for txt in rev:
+        profile = personality_insights.profile(
+            txt.encode('utf8'), content_type='text/plain',
+            raw_scores=True, consumption_preferences=True)
+        result.append(to_vector(profile))
     return result
 
 
 def classify_review(com):
-    input_file = open(com + '.json').read()
-    reviews = json.loads(input_file)
     result = []
     for i in range(0,5):
-        result.append('')
-    for review in reviews:
+        input_file = open('./data/' + com + '_' + str(i+1) + '.json').read()
+        reviews = json.loads(input_file)
         txt = ''
-        if review['advice'] != None:
-            txt += review['advice']
-        if review['cons'] != None:
-            txt += review['cons']
-        if review['pros'] != None:
-            txt += review['pros']
-        if review['summary'] != None:
-            txt += review['summary']
-        result[review['rating'] - 1] += txt
+        for review in reviews:
+            if review['advice'] != None:
+                txt += '' + review['advice']
+            if review['cons'] != None:
+                txt += '' + review['cons']
+            if review['pros'] != None:
+                txt += '' + review['pros']
+            if review['summary'] != None:
+                txt += '' + review['summary']
+        result.append(txt)
     return result
 
 if __name__ == "__main__":
-    min_words = 100
-    companies = ['Citi']
+    companies = ['Facebook', 'Google', 'LinkedIn']
     dict = {}
     for company in companies:
         temp = classify_review(company)
         vector = get_personality(temp)
-        dict.update({company : vector})
+        dict.update({company: vector})
     json_export(dict)
